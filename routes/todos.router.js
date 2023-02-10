3bddc2234451ba2ccc1a1d780c65dae7f1a1d8e0
Hyunsoo <kaist217@naver.com>
2023-02-10 16:34:03 +0900

    할 일 목록 가져오고, 할 일 순서 변경하는 API 만들기

--- routes/todos.router.js
+++ routes/todos.router.js
@@ -21,4 +21,36 @@ router.post('/todos', async (req, res) => {
   res.send({ todo });
 });
 
+router.get('/todos', async (req, res) => {
+  const todos = await Todo.find().sort('-order').exec(); // 특정 조건 입력 X일 경우, 모든 데이터 조회 / order앞에 -붙이면 내림차순!
+
+  res.send({ todos });
+});
+
+router.patch('/todos/:todoId', async (req, res) => {
+  const { todoId } = req.params;
+  const { order } = req.body;
+
+  // 1. todoId에 해당하는 할 일이 있는가?
+  // 1-1. todoId에 해당하는 할 일이 없으면, 에러를 출력해야 함.
+  const currentTodo = await Todo.findById(todoId);
+  if (!currentTodo) {
+    return res
+      .status(400)
+      .json({ errorMessage: '존재하지 않는 할 일 입니다.' });
+  }
+
+  if (order) {
+    const targetTodo = await Todo.findOne({ order }).exec();
+    if (targetTodo) {
+      targetTodo.order = currentTodo.order;
+      await targetTodo.save(); // 2번째 -> 1번째 변경 / 바꾸려는 대상이 존재한다면, 그것부터 변경하고,
+    }
+    currentTodo.order = order;
+    await currentTodo.save(); // 1번째 -> 2번째 변경 / 그 다음에 자신을 수정하기.
+  }
+
+  res.send();
+});
+
 module.exports = router;
